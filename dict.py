e78eb9af2c1958f02ae040cc25c433752769c003
farhanas82 <farhanas82@users.noreply.github.com>
2014-05-20 16:21:10 -0700

    updatedic.py
    added two more phonebook.

--- dict.py
+++ dict.py
@@ -3,8 +3,10 @@ phonebook={
 	"suresh":35758085,
 	"parnitha":6457485094,
 	"Renuga":4867846495
+	"XYZ":4883673777"
+	"ABC":3344564555"
 	}
-phonebook.update({"keerthan":736438})
-
+phonebook.update({"XYZ"})
+phonebook.update("ABC")}
 for name,number in phonebook.iteritems():
 	print "phone number of %s is %d"%(name,number)
